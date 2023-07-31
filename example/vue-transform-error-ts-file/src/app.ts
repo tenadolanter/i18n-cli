4d0768bfe97ffda5ffce8b8aa16ba1c66979075a
tenado <690784374@qq.com>
2023-07-31 17:01:56 +0800

    1

---
+++ example/vue-transform-error-ts-file/src/app.ts
@@ -0,0 +1,33 @@
+export const RewardStartEmpPage = [
+  {
+      "id": "11384750486160129",
+      "name": "员工姓名",
+      "color": "#0073EA",
+      "taskList": [{
+          "id": "11700028291874816",
+          "employeeId": 2,
+          "orgId": "11700028275458048",
+          "employeeNumber": "YL000002",
+          "employeeName": "周兴智",
+          "orgName": "测试一组",
+          "orgNumber": "yz001",
+          "positionId": null,
+          "positionName": null,
+          "positionNumber": null,
+          "orgParentId": "11700028273262592",
+          "orgParentNumber": "YL039",
+          "orgParentName": "QA组",
+          "assignorId": "20",
+          "assignorNumber": "YL000020",
+          "assignorName": "许毅超",
+          "authorizers": null,
+          "rewardId": null,
+          "stageHandlerId": "20",
+          "stageHandlerNumber": "YL000020",
+          "stageHandlerName": "许毅超",
+          "stageStatus": null,
+          "stageName": "分配填报"
+      }],
+      "sort": 1
+  }
+]
