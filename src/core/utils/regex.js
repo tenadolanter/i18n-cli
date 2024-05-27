fd95d99023118cda35d9a2b9cbc473c88e2c8eaa
tenado <690784374@qq.com>
2024-05-27 20:05:20 +0800

    fix: 修复匹配自闭和标签时，属性包含大于号和小于号时匹配的bug

--- src/core/utils/regex.js
+++ src/core/utils/regex.js
@@ -14,15 +14,18 @@ module.exports = {
   // ></Test>
   htmlTagWithUppercaseChar: /(<\/?)(([a-z][a-z0-9-]+)?[A-Z][a-zA-Z0-9-]*)/g,
   // 匹配所有包含大写字母的属性，例如<div :Ysss="name">或<div @Ysss="name">
-  htmlAttributeWithUppercaseChar: /(?<=\s|^)((@?)(:?)([a-z][a-z0-9-]*)?[A-Z][a-zA-Z0-9-]*)(?=[=\s>])/g,
+  htmlAttributeWithUppercaseChar:
+    /(?<=\s|^)((@?)(:?)([a-z][a-z0-9-]*)?[A-Z][a-zA-Z0-9-]*)(?=[=\s>])/g,
   // 匹配包含大写字母的插槽，例如<div #headToolbar></div>
-  htmlSlotWithUppercaseChar: /(?<=\s|^)(#([a-z][a-z0-9-]+)?[A-Z][a-zA-Z0-9-]*)(?=[=\s>])/g,
+  htmlSlotWithUppercaseChar:
+    /(?<=\s|^)(#([a-z][a-z0-9-]+)?[A-Z][a-zA-Z0-9-]*)(?=[=\s>])/g,
   // 匹配自闭和标签
-  htmlAutoCloseTag: /<([a-z][a-z0-9-]*)(?:[^>]*?)?\/>/g,
+  htmlAutoCloseTag:
+    /<([a-z][a-z0-9-]*)(\s+[^<>\s]*("[^"]*"|'[^']*'|[^<>'"\s]*)?)*\s*\/>/g,
   // 匹配<template或</template
   htmlTemplateTag: /(<\/?)(template)/g,
   htmlTemplateStartTag: /(<template)/g,
   htmlTemplateEndTag: /(<\/template)/g,
   // 匹配html闭和标签
   htmlTagAutoClose: /<\/?.+?>/g,
-}
+};
