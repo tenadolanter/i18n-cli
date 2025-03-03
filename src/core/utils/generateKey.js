d22eec41598b3c4a995da8eaecc9478d748e5fca
Hans Chan <icsbun@gmail.com>
2025-03-03 17:37:54 +0800

    keygenStrategy 支持使用原文

--- src/core/utils/generateKey.js
+++ src/core/utils/generateKey.js
@@ -3,20 +3,22 @@
  * @param { String } text - 需要翻译的文本
  * @param { Object } options - 配置
  */
-const { nanoid } = require("nanoid");
-const limax = require("limax");
+const { nanoid } = require('nanoid');
+const limax = require('limax');
 module.exports = function generateKey(text, options) {
-  let result = "";
+  let result = '';
   const { keyPrefix, keygenStrategy } = options;
   // 生成随机字符串id
-  if (keygenStrategy === "random") {
+  if (keygenStrategy === 'random') {
     result = nanoid();
+  } else if (keygenStrategy === 'original') {
+    result = text;
   }
   // 转换
   // i ♥ latin > i-love-latin
   // 我爱官话 > wo-ai-guan-hua
   else {
-    text = text.replace(/\$/g, "");
+    text = text.replace(/\$/g, '');
     result = limax(text, { tone: false });
   }
 
